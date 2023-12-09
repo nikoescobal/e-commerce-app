369c5853971a3de7fd6c9f0ff6f37a9a264be4df
Nikolas Escobal <niko.escobal@gmail.com>
2023-12-10 01:14:04 +0800

    Added HomePage component

---
+++ app/home/page.js
@@ -0,0 +1,11 @@
+// import styles from './page.module.css';
+import styles from '../../styles/globals.scss';
+import Home from '../page';
+
+export default function HomePage() {
+  return (
+    <main className={styles.main}>
+      <Home />
+    </main>
+  );
+}
