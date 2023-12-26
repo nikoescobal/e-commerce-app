956183905f760145f0ff9d0479d6e23b2ca6d143
Nikolas Escobal <niko.escobal@gmail.com>
2023-12-26 18:38:44 +0800

    added Manrope to layout

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,13 @@
 import './globals.css';
+import { Manrope } from 'next/font/google';
+
+// components
 import Header from '../components/organisms/Header/Header';
 import Footer from '../components/organisms/Footer/Footer';
 import AboutUsMini from '../components/organisms/AboutUsMini/AboutUsMini';
 // import styles from '../components/organisms/Header/Header.module.scss';
+
+const manrope = Manrope({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Audiophile',
@@ -12,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={manrope.className}>
         <Header />
         {children}
         <AboutUsMini />
