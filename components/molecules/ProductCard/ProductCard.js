9d63201a4341076b12aaa123de0ae7c00cc84a82
Nikolas Escobal <niko.escobal@gmail.com>
2023-12-26 18:41:12 +0800

    Updated ProductCard and styles

--- components/molecules/ProductCard/ProductCard.js
+++ components/molecules/ProductCard/ProductCard.js
@@ -1,13 +1,52 @@
+'use client';
+
 // components/molecules/ProductCard/ProductCard.js
 import React from 'react';
 import Image from '../../atoms/Image/Image';
 import styles from './ProductCard.module.scss';
 import Typography from '../../atoms/Typography/Typography';
 import Button from '../../atoms/Button/Button';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
+import Dialog from '../Dialog/Dialog';
 
-const ProductCard = ({ item }) => {
+const ProductCard = ({
+  item,
+  hasAddToCart,
+  hasSeeProduct,
+  isDetailedView,
+  price,
+  addToCart,
+}) => {
+  const cardClass = classNames(styles.wrapper, {
+    [styles.detailed]: isDetailedView,
+  });
+
+  const addToCartFunction = (item) => {};
+
+  async function onClose() {
+    // 'use server';
+    console.log('modal has closed');
+  }
+
+  async function onOk() {
+    // 'use server';
+    console.log('Ok was clicked');
+  }
+
+  const id = item.id;
+  const pathname = usePathname();
   return (
-    <div className={styles.wrapper}>
+    <div className={cardClass}>
+      <Dialog title="Example Modal" onClose={onClose} onOk={onOk}>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
+          tenetur ratione vitae molestiae accusamus, nesciunt consectetur ea
+          corporis quo placeat deserunt hic, eos repudiandae veniam labore
+          libero, sint nemo voluptatibus.
+        </p>
+      </Dialog>
       <picture className={styles.categoryImage}>
         <source media="(min-width: 1024px)" srcSet={item.imageUrl.desktop} />
         <source media="(min-width: 768px)" srcSet={item.imageUrl.tablet} />
@@ -43,7 +82,40 @@ const ProductCard = ({ item }) => {
         >
           {item.description.trim()}
         </Typography>
-        <Button className={styles.button}>{item.buttonText}</Button>
+        {isDetailedView && (
+          <>
+            <Typography variant="h6" className={styles.price}>
+              ${item.price}
+            </Typography>
+            <div className={styles.addToCartWrapper}>
+              <div className={styles.counterWrapper}>
+                <button className={styles.operatorButton}>-</button>
+                {/* <input type="number" className={styles.counter} /> */}
+                <div type="number" className={styles.counter}>
+                  1
+                </div>
+                <button className={styles.operatorButton}>+</button>
+              </div>
+              {/* <Button
+                className={styles.addToCartButton}
+                onClick={() => setOpenModal(true)}
+              >
+                Add to Cart
+              </Button> */}
+              <Button
+                className={styles.addToCartButton}
+                onClick={() => setOpenModal(true)}
+              >
+                Add to Cart
+              </Button>
+            </div>
+          </>
+        )}
+        <Link href={`${pathname}/${id}`}>
+          {hasSeeProduct === true && (
+            <Button className={styles.button}>{item.buttonSP}</Button>
+          )}
+        </Link>
       </div>
     </div>
   );
